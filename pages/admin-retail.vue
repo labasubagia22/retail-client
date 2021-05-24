63e1a678dccb891628287a42b64e1d512c6cd9cb
Laba Subagia <labasubagia22@gmail.com>
2021-05-24 21:02:50 +0800

    [update] admin retail

--- pages/admin-retail.vue
+++ pages/admin-retail.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="w-full h-full flex text-gray-600">
-    <sidebar :menu="menu" class="w-2/12 z-10" />
+    <!-- sidebar -->
+    <sidebar :menu="menu" class="w-2/12 z-10 fixed" />
+    <div class="w-2/12"></div>
+
+    <!-- container -->
     <div class="w-10/12">
-      <div class="p-4 px-6 shadow-md bg-gray-100 text-gray-600">
+      <div class="p-4 px-6 shadow-md bg-gray-100 text-gray-600 fixed w-full">
         {{ title }} Page
       </div>
-      <NuxtChild class="p-6" />
+
+      <!-- content -->
+      <NuxtChild class="p-6 mt-14" />
     </div>
   </div>
 </template>
