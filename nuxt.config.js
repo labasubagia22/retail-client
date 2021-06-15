32dc65a1346de73184f4fccbab0622ae95a6f5e9
Laba Subagia <labasubagia22@gmail.com>
2021-06-16 00:42:54 +0800

    use env

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://mahabharata-retail-server.herokuapp.com',
+    baseURL: process.env.BASE_URL || 'http://localhost:8000',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
